85b96f58a4d91f9023f3be9d3d0a312f981686d1
gz-ted <gnatuf@163.com>
2018-06-15 11:54:37 +0800

    add dist

---
+++ dist/precache-manifest.0da9fc6240a75b0b3db71da74dfbd250.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d28559a43ff74a28b535",
+    "url": "vendors.js"
+  },
+  {
+    "revision": "53239ac677908ada40e3",
+    "url": "runtime.js"
+  },
+  {
+    "revision": "e96fc9a4188eab986e29",
+    "url": "main.js"
+  },
+  {
+    "revision": "b85c0eb63b8ad6536638b5c48806886f",
+    "url": "index.html"
+  }
+];
