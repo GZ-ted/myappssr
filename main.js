64361d5ea6d6fe3c63cc1e2fbb5de330b95ad26b
tagfoo <gnatuf@163.com>
2018-06-15 11:28:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /*客户端入口*/
 
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import Home from './components/home.js';
 
+//ssr，用hydrate代替render
 hydrate(<Home /> , document.getElementById('app'));
